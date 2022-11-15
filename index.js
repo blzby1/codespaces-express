df35597e1e95309ab719fc18886da5fa5ae690a5
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-11-15 14:54:47 +0000

    make a basic site

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const subdomain = require('express-subdomain');
+const fs = require('fs');
+const http = require('http')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+var app = express();
+var router = express.Router();
+
+const httpServer = http.createServer(app, router)
+
+
+
+app.use("/", bodyParser.urlencoded({extended: false}));
+app.use('/public', express.static(__dirname + '/public'));
+
+router.get('/:taker', function(req, res) {
+    res.json({
+        message: "this is a test, nothing is here"
+    });
+});
+
+app.use(subdomain('sv', router));
+
+codes = ["demo", "69iscooll", "ronana", "password", "chickentender", "derekgaming", "wgeoiscool"]
+
+app.route('/').get(function(req, res) {
+    res.sendFile(__dirname + "/views/index.html")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/log', function(req, res) {
+    var funnyip = req.ip
+    res.json({
+        ip: funnyip
+    });
+    console.log(req.method, funnyip);
+});
+
+const HTTP_PORT = 2453
+
+httpServer.listen(HTTP_PORT, function() {
+    console.log('HTTP server running on port ' + HTTP_PORT);
+	console.log(codes);
+});
